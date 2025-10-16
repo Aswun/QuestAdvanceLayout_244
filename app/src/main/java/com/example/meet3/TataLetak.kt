41786fb49bd5207c7b325097aa953759077b87d7
Aswun <aswinlutfian32@gmail.com>
2025-10-16 14:23:08 +0700

    menambahkan function tata letak row column, baris 2

--- app/src/main/java/com/example/meet3/TataLetak.kt
+++ app/src/main/java/com/example/meet3/TataLetak.kt
@@ -80,5 +80,11 @@ fun TatatatakRowColumn(modifier: Modifier = Modifier) {
             Text(text = "Komponen2Baris1")
             Text(text = "Komponen3Baris1")
         }
+        //Baris2
+        Column() {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
     }
 }
