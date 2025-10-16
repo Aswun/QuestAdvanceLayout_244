eb6bf98ffef0ea2e08430eb86f95121f6c01462f
Aswun <aswinlutfian32@gmail.com>
2025-10-16 15:16:53 +0700

    fix MainActivity.kt

--- app/src/main/java/com/example/meet3/MainActivity.kt
+++ app/src/main/java/com/example/meet3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.meet3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -10,16 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 import com.example.meet3.ui.theme.Meet3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             Meet3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // Panggil composable layout utama dengan padding dari Scaffold
                     TataletakBoxColumnRow(
                         modifier = Modifier.padding(paddingValues = innerPadding)
                     )
