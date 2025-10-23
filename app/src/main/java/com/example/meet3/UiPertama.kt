7342e42e7c1b5b2be6ea40946c4da51a24e5d954
Aswun <aswinlutfian32@gmail.com>
2025-10-23 09:14:51 +0700

    menambahkan row

--- app/src/main/java/com/example/meet3/UiPertama.kt
+++ app/src/main/java/com/example/meet3/UiPertama.kt
@@ -44,5 +44,14 @@ fun ActivitasPertama() {
                 .padding(all = 12.dp),
             colors = CardDefaults.cardColors(containerColor = Color.DarkGray)
         )
+        {
+            Row() {
+                val gambar = painterResource(id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
+                )
+        }
     }
 }
