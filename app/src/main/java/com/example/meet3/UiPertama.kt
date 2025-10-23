2443e3f373fedf91ad588f7e0b3b2af2ac1824e2
Aswun <aswinlutfian32@gmail.com>
2025-10-23 09:15:23 +0700

    menambahkan spacer untuk row

--- app/src/main/java/com/example/meet3/UiPertama.kt
+++ app/src/main/java/com/example/meet3/UiPertama.kt
@@ -52,6 +52,7 @@ fun ActivitasPertama() {
                     contentDescription = null,
                     modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
                 )
-        }
+                Spacer(modifier = Modifier.width(width = 30.dp))
+            }
     }
 }
