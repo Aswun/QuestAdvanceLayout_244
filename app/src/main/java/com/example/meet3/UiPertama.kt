a762e6ebe944aafaaf331cb531fc51574364e7f3
Aswun <aswinlutfian32@gmail.com>
2025-10-23 09:12:18 +0700

    menambahkan text dengan stringresource prodi

--- app/src/main/java/com/example/meet3/UiPertama.kt
+++ app/src/main/java/com/example/meet3/UiPertama.kt
@@ -29,6 +29,10 @@ import androidx.compose.ui.text.font.FontFamily
 fun ActivitasPertama() {
     Column(modifier = Modifier.padding(top = 100.dp).fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally) {
-
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
