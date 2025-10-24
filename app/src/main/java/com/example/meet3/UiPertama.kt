25d87309f4aad7787f05748029b5f2494399639d
Aswun <aswinlutfian32@gmail.com>
2025-10-24 20:19:26 +0700

    menambahkan text untuk nama pada card ketiga

--- app/src/main/java/com/example/meet3/UiPertama.kt
+++ app/src/main/java/com/example/meet3/UiPertama.kt
@@ -124,7 +124,14 @@ fun ActivitasPertama(modifier: Modifier) {
                     .padding(5.dp)
             )
             Spacer(modifier = Modifier.width(30.dp))
-
+            Column {
+                Text(
+                    text = "Isi Nama",
+                    fontSize = 25.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 15.dp)
+                )
+            }
         }
 
         Box(modifier = Modifier.fillMaxSize()) {
